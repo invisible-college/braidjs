970ef0f1094dcb21a6c9be37e570f8f086eef188
Michael Toomim <toomim@gm.com>
2019-08-16 13:11:27 -0700

    added basic server http2 code

--- server.js
+++ server.js
@@ -253,6 +253,76 @@ function import_server (bus, options)
                             })
 
     },
+    h2_server: function h2_server(client_bus_func) {
+        var master = this
+        var http = require('http2')
+        var free_the_cors = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        }
+
+        const server = http.createSecureServer({
+            key: fs.readFileSync(master.options.certs.private_key),
+            cert: fs.readFileSync(master.options.certs.cert)})
+
+        server.on('error', (error) => {console.error(error)})
+
+        server.on('session', function (session) {
+            console.log("New Session!")
+            session.on('close', () => console.log("Session closed"))
+            session.on('goaway', () => console.log("Session goaway-ed"))
+            session.on('error', (e) => console.log("Session errored:", e))
+            session.on('timeout', () => console.log("Session timed out"))
+        })
+
+        server.on('request', function (req, res) {
+            console.log("Got a request: {");
+            for (let [k, v] of Object.entries(req.headers))
+                console.log(`  ${k}: "${v}"`)
+            console.log("}")
+
+            switch (req.method) {
+            case 'GET':
+                respond_sse(res.stream, req.url);
+                break
+            case 'OPTIONS':
+                console.log('## We got the options~!!')
+                res.stream.respond({':status': 200, ...free_the_cors})
+                res.stream.end("forgotten")
+                break
+            case 'FORGET':
+                if (!res.stream.session.open_gets[req.url]) {
+                    res.stream.respond({':status': 404});
+                    res.stream.end();
+                } else {
+                    res.stream.session.open_gets[req.url].close()
+                    res.stream.respond({':status': 200, ...free_the_cors})
+                    res.stream.end("forgotten");
+                    delete res.stream.session.open_gets[req.url]
+                }
+                break
+            case 'PUT':
+            case 'PATCH':
+                console.log("Got a set: {");
+                for (let [k, v] of Object.entries(req.headers))
+                    console.log(`  ${k}: "${v}"`)
+                console.log("}")
+                res.stream.respond({':status': 200, ...free_the_cors})
+                res.stream.end("ack");
+                break
+            case 'DELETE':
+                res.stream.respond({':status': 200, ...free_the_cors});
+                res.stream.end("deeleeted de-1337-ed");
+                break
+            default:
+                res.stream.respond({':status': 404})
+                res.stream.end()
+                break
+            }
+        })
+        server.listen(PORT, () => console.log(`Server listening!`));
+    },
     sockjs_server: function sockjs_server(httpserver, client_bus_func) {
         var master = this
         var client_num = 0
